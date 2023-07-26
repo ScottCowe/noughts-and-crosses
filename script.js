e743b95becf2bec378d123ba10422007f771a34c
MisterCow1 <scott@colquhoun.com>
2023-07-26 13:44:53 +0100

    Fixed shitty code

--- script.js
+++ script.js
@@ -54,6 +54,60 @@ function allElementsMatch(array) {
   return true
 }
 
+function tooDifyBoard(boardArray, boardLength) {
+  let array = new Array(boardLength)
+
+  for(let i = 0; i < boardLength; i++) {
+    array[i] = new Array(boardLength)
+    for(let j = 0; j < boardLength; j++) {
+      array[i][j] = board[i*boardLength + j]
+    }
+  }
+
+  return array
+}
+
 function getMoveResult(board) {
-// 0 => Continue, 1 => Noughts win, 2 => Crosses win, 3 => Draw
+  // 0 => Continue, 1 => Noughts win, 2 => Crosses win, 3 => Draw
+  
+  // Turn board array into 2D array
+  // get horizontal and vertical slices
+  // get diagonal slices
+  // add slices to array
+
+  // for each slice in slices check if winner
+  // if so, first elem in array determines noughts or crosses
+  
+  const array = tooDifyBoard(board, boardWidth)
+  let slices = []
+  
+  // Horizontal slices
+  for(let i = 0; i < boardWidth; i++) {
+    slices.push(array[i])
+  }
+  
+  // Vertical slices
+  for(let i = 0; i < boardWidth; i++) {
+    let slice = []
+    for(let j = 0; j < boardWidth; j++) {
+       slice.push(array[j][i])
+    }
+    slices.push(slice)
+  }
+
+  // Descending diagonal
+  let descSlice = []
+  for(let i = 0; i < boardWidth; i++) {
+    descSlice.push(array[i][i])
+  }
+  slices.push(descSlice)
+
+  // Ascending diagonal
+  let ascSlice = []
+  for(let i = 0; i < boardWidth; i++) {
+    ascSlice.push(array[boardWidth - 1 - i][i])
+  }
+  slices.push(ascSlice)
+
+  return 0
 }
